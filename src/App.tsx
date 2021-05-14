0040a5c499f8fb2cfcef204f65f050c7ae43ff2b
Twince <tjsdn811@gmail.com>
2021-05-14 23:12:33 +0900

    faet: refector

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
+import Text from './Text';
 
 const App: React.FC = () => {
     //FC는 Function Componet즉 함수형
     return (
         <>
-            <p style={{
-                fontWeight: "bold",
-                color: "blue",
-            }}>
-                Hello World</p>
+            <Text color="blue">A!</Text>
         </>
     );
 };
