5472fe6eeb431c76a4ae4c2623adf23562fec498
Twince <tjsdn811@gmail.com>
2021-05-21 22:52:44 +0900

    feat: Props Child 설정 및 TypeScript 제네릭 설정

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,22 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
-interface TextProps{
-    readonly color:string;
+interface TextProps {
+    readonly color: string;
+    readonly onClick?: React.MouseEventHandler;
+    // onClick이 필요없을수도 있으므로 ?로 옵션으로 선택해줄 수 있음.
 }
 
-const Text: React.FC<TextProps> = ({ children, color }) => {
+const ColorText = styled.p<{ a: string }>`
+    /* TypeScript 제네릭 */
+    font-weight: bold;
+    color: ${(props) => props.a};
+    user-select: none;
+
+    &:hover{
+      font-weight: normal;
+    }
+`;
+
+const Text: React.FC<TextProps> = ({ children, color, onClick }) => {
     //FC는 Function Componet즉 함수형
     return (
         <>
-            <p
-                style={{
-                    fontWeight: "bold",
-                    color,
-                }}
-                //key와 vlaue가 같으므로 color로 표시(원래는 color: color)
-            >
-                글자 : {children}
-            </p>
+            <ColorText a={color} onClick={onClick}>글자 : {children}</ColorText>
         </>
     );
 };
