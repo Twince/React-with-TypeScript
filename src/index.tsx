58ff5bba893f720d2e27e6d468c8f84b56411fd8
Twince <tjsdn811@gmail.com>
2021-05-28 22:51:44 +0900

    feat: custom hooks provider 사용

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import B from "./b";
+import { NumProvider } from './hooks/useNum';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" component={App} exact />
-                <Route path="/b" component={B} exact />
-                {/* exact 옵션은 path에 있는 주소와 완전히 일치애햐 라우팅 되게 하는 역할을 해줌. */}
-            </Switch>
-        </BrowserRouter>
+        <NumProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={App} exact />
+                    <Route path="/b" component={B} exact />
+                    {/* exact 옵션은 path에 있는 주소와 완전히 일치애햐 라우팅 되게 하는 역할을 해줌. */}
+                </Switch>
+            </BrowserRouter>
+        </NumProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
